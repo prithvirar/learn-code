5817c7bf6c2b190494dc21102e4c8ccd5fa69941
prithvirar <prithvirajmandal4529@gmail.com>
2026-09-20 00:12:47 +0530

    done practicing

--- python/notep.py
+++ python/notep.py
@@ -901,24 +901,32 @@ until loop ends"""
 #                      return
 #                 line += 1
 #             return -1
-
 # checkforline()
 
-count = 0
-with open("evennum.txt","r") as f:
-     data = f.read()
-     print(data)
 
-     nums = data.split(",")
-    #  print(nums)
-     for val in nums:
+"""to get data in list from file"""
+# with open("evennum.txt","r") as f:
+#      data = f.read()
+#      print(data)
+
+#      num = ""
+#      for i in range(len(data)):
+#           if(data[i]==","):
+#                print(int(num))
+#                num=""
+#           else:
+#                num += data[i]
+
+def counteven():
+     count = 0
+     with open("evennum.txt","r") as f:
+        data = f.read()
+        print(data)
+
+        nums = data.split(",")
+        # print(nums)
+        for val in nums:
           if(int(val)%2 == 0):
                count+=1
-print(count)
-    #  num = ""
-    #  for i in range(len(data)):
-    #       if(data[i]==","):
-    #            print(int(num))
-    #            num=""
-    #       else:
-    #            num += data[i]
+     print(count)
+counteven()
